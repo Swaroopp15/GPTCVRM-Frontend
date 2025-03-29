1b4167f2f357255b0047997658fbb7e30c64fd21
jayanth-14 <jayanthpetchetti@gmail.com>
2025-03-29 19:33:54 +0530

    implement dynamic hero

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -3,15 +3,18 @@ import DropDown from './DropDown'
 import Drop from '../../../assets/icons/Drop'
 import { Context } from '../../../../Context/Context'
 import objectToArray from '../../../functions/objectsToArray';
+import { Link } from 'react-router';
 
 function Navbar() {
   const {committees, departmentNames} = useContext(Context);
   return (
     <nav class="mx-5 flex flex-row gap-4 text-gray-700">
-  <p>Home</p>
-  <p>About</p>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link>
   <DropDown name={"departments"} values={objectToArray(departmentNames)} link={"/department/"} />
   <DropDown name={"committees"} values={objectToArray(committees)} link={"/committee/"} />
+      <Link to={"/results"}>Results</Link>
+      <Link to={"/placements"}>Placements</Link>
   <DropDown name={"others"} values={["placements", "results"]} />
 </nav>
 
