8efab9e1e1f7da2e60ff2374ccf0372a160006b2
jayanth-14 <jayanthpetchetti@gmail.com>
2025-05-10 14:23:00 +0530

    implement creating new Notification

--- src/functions/notification.js
+++ src/functions/notification.js
@@ -1,11 +1,32 @@
-const getNotifications = async() => {};
-const addNotification = async() => {};
-const deleteNotification = async() => {};
-const updateNotification = async() => {};
+const getNotifications = async () => {};
+const addNotification = async (notification) => {
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_BACKEND + "notifications",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(notification),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to create notification");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error creating notification:", error);
+    alert("Failed to create notification");
+  }
+};
+const deleteNotification = async () => {};
+const updateNotification = async () => {};
 
-export{
+export {
   getNotifications,
   addNotification,
   deleteNotification,
-  updateNotification
-}
+  updateNotification,
+};
